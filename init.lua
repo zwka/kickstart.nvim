7224c133d901ffc32e534e862f2407422fa41660
Alvin Zing <zwka@Alvins-MacBook-Air.local>
2025-11-09 07:17:16 +0800

    added sudormrfbin/cheatsheet plugin

--- init.lua
+++ init.lua
@@ -248,6 +248,8 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  -- NOTE: zwka plugins
+  'sudormrfbin/cheatsheet.nvim', -- A searchable cheatsheet for neovim within the editor
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
